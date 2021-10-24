221ca4885533f047518d3c9543e3945a6f2b1149
Kkomek21 <kkomek21@gmail.com>
2021-10-24 17:24:12 +0200

    Part 10 1/2 - Create, Update and Delete Posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def home(request):
@@ -8,6 +9,21 @@ def home(request):
 	}
 	return render(request, 'blog/home.html', context)
 
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+	model = Post
+
+
+class PostCreateView(CreateView):
+	model = Post
+	fields = ['title', 'content']
 
 def about(request):
 	return render(request, 'blog/about.html')
